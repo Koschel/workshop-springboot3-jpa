7fb5b161768880eab9c15f2d32e48daf23d9885b
Diego Koschel <dasksoethe1@gmail.com>
2024-11-06 21:56:01 -0300

    Order, Instant, ISO 8601 p2

--- src/main/java/com/cursojavaspring/course/entities/Order.java
+++ src/main/java/com/cursojavaspring/course/entities/Order.java
@@ -3,6 +3,10 @@ package com.cursojavaspring.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,18 +20,20 @@ import jakarta.persistence.Table;
 public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
+
 	public Order() {
-		
+
 	}
 
 	public Order(Long id, Instant moment, User client) {
@@ -85,8 +91,5 @@ public class Order implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
-	
+
 }
